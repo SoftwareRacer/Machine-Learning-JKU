62030dfa09ec3cb66627571f7a50275d6aa8168c
Marco Hennermann <79541316+SoftwareRacer@users.noreply.github.com>
2021-05-03 11:35:16 +0200

    Update ex4.py

--- as2/ex4.py
+++ as2/ex4.py
@@ -30,32 +30,32 @@ list_of_incorrect_files = []
                 ex2(old_path, out_dir, logfile) #if path links to directory, reexecute the function for this folder
             if not os.path.isfile(out_dir + "/" + str(ser_num).zfill(7)) and (old_path.endswith(".jpeg") or old_path.endswith(".jpg") or old_path.endswith(".JPG") or old_path.endswith(".JPEG")): #1. condition
          '''   
-def ex4(image_array, border_x, border_y):
+def ex4(image_array, border_x, border_y): # function to make part of image black which should be reconstructed later by machine learning
     try:
         input_array = image_array
         target_array = []
-        for i in range(0, border_x[0]):
+        for i in range(0, border_x[0]): # border_x[0] describes upper horizontal border
+            for j in range(0, len(input_array[1,:])): 
+                input_array[i][j] = 0 # sets each pixel on upper rows until border to 0
+                target_array += image_array[i][j]
+        for i in range(len(input_array[:,1])-border_x[1], len(input_array[:,1])): # border_x[1] describes lower horizontal border
             for j in range(0, len(input_array[1,:])):
-                input_array[i][j] = 0
+                input_array[i][j] = 0 # sets each pixel on lower rows from border to 0
                 target_array += image_array[i][j]
-        for i in range(len(input_array[:,1])-border_x[1], len(input_array[:,1])):
-            for j in range(0, len(input_array[1,:])):
-                input_array[i][j] = 0
-                target_array += image_array[i][j]
-        for i in range(0, border_y[0]):
+        for i in range(0, border_y[0]): # border_y[0] describes left vertical border
             for j in range(0, len(input_array[:,1])):
-                input_array[j][i] = 0
+                input_array[j][i] = 0 # sets each pixel on left columns until border to 0
                 target_array += image_array[i][j]
-        for i in range(len(input_array[1,:])-border_y[1], len(input_array[1,:])):
+        for i in range(len(input_array[1,:])-border_y[1], len(input_array[1,:])): # border_y[1] describes right vertical border
             for j in range(0, len(input_array[:,1])):
-                input_array[j][i] = 0
+                input_array[j][i] = 0 # sets each pixel on right columns from border to 0
                 target_array += image_array[i][j]
        
-        known_array = input_array
+        known_array = input_array # creates new array which only contents 0 or 1 in each pixel to say if the pixel is known or not
         for i in range(len(known_array[:,1])):
             for i in range(len(known_array[1,:])):
                 if(known_array[i][j] != 0):
-                    known_array[i][j] = 1
+                    known_array[i][j] = 1 # sets every not unknown pixel to 1
     except NotImplementedError as nie:
         print("image_array is not a 2D-numpy-array")
         logging.error(nie)
